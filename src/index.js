8dde8b730d1cb60fae08f7bb680f7aabceb6b63e
maodongbang <maodongbang@jd.com>
2020-11-10 10:24:07 +0800

    react组合使用props、state、生命周期

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default class Test extends Component{
+  constructor(props){
+    super(props)
+    console.log("this.props====",this.props)
+    this.state = {
+      department: '',
+      input: ''
+    }
+  }
+   
+  componentDidMount(){
+    const {dept} = this.props
+    let x = []
+    dept.map((v,k)=>
+      // console.log("v=====",v)
+        x.push(v)
+    )
+
+    this.setState({
+      department: x
+    })
+  }
+
+  handleAdd=(e)=>{
+    console.log("e=====",e.target.value)
+    this.setState({
+      input :e.target.value
+    })
+  };
+
+
+  addDempartment=()=>{
+    let x = this.state.department
+    x.push(this.state.input)
+    Object.keys(x).map((k)=>
+    console.log(x[k]))
+    this.setState({
+      department:x,
+      input: ''
+    })
+  }
+
+  render() {
+    
+      const {department,input} = this.state
+      const listItems = Object.keys(department).map((dept)=>
+                  <div >
+                   <li>{this.state.department[dept]}</li>
+                  </div>
+      )
+    return(
+
+    <div  style ={{marginTop: '100px',marginLeft:'40%'}}> 
+       <input type='text' placeholder ='添加部门' onChange ={this.handleAdd} value = {this.state.input}/> 
+        <button onClick = {this.addDempartment}  style ={{background:'#F0FFFF'}}>添加</button><br></br><br></br>
+       <span style ={{fontWeight: 'bolder'}}>公司部门</span>
+       <ol style={{marginLeft: '20px'}}>{listItems}</ol>
+    </div>
+    )  
+  }
+}
+
+const dept = ['平台中心','研发部']
+ReactDOM.render(<Test dept={dept}/>,document.getElementById('root'))
